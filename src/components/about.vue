b74b4762e10bb330517a8ab36a1f43b26fc49528
ivancis <diazianluis@gmail.com>
2019-12-17 21:17:07 +0100

    multiple toggles

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,55 +1,48 @@
 <template>
-    <section class="section section--about">
-        <!-- <div class="gallery-element gallery-element--about">
-            <p>
-                About me
-            </p>
-        </div> -->
-        <div class="grido">
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
-                </div>
-            </div>
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
-                </div>
-            </div>
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
-                </div>
-            </div>
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
-                </div>
-            </div>
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
-                </div>
-            </div>
-            <div class="grido__item">
-                <div class="about-card">
-                    <div class="about-card__cover">
-                        <span class="icon">X</span>
-                    </div>
+    <div class="grido">
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
                 </div>
             </div>
         </div>
-    </section>
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
+                </div>
+            </div>
+        </div>
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
+                </div>
+            </div>
+        </div>
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
+                </div>
+            </div>
+        </div>
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
+                </div>
+            </div>
+        </div>
+        <div class="grido__item">
+            <div class="about-card">
+                <div class="about-card__cover">
+                    <span class="icon">X</span>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,96 @@
 <template>
     <div class="page page--home">
         <section class="section section--intro">
-            <div class="column">
-                <h2>Hello, my name is <b>Iván.</b></h2>
-                <h2>I’m a <b>graphic and UI designer</b>, passionate about UX.</h2>
-                <h2>Also an experienced <b>UI developer</b>, aiming to be full front-end.</h2>
-                <div class="nav">
-                    <span class="nav__text">Here you'll find more</span>
-                    <a
-                        v-on:click="component = 'about'"
-                        class="nav__link"
-                    >
-                        <span class="nav__text">about me</span>
-                    </a>
-                    <span class="nav__text">and some of</span>
-                    <a
-                        v-on:click="component = 'works'"
-                        class="nav__link"
-                    >
-                        <span class="nav__text">my works</span>
-                    </a>
-                </div>
+            <div class="intro-text">
+                <h2>Hello, I'm <b v-on:click="component = 'about'">Iván </b><em>Díaz Bonansea</em></h2>
+                <h2>this is my showcase on the internet.</h2>
             </div>
         </section>
-
-        <component v-bind:is="component" />
+        <section class="section section--content">
+            <div class="intro-text">
+                <h4>I’m a <b>graphic designer,</b> specialized in <b>digital UI and UX.</b></h4>
+                <h4>Also I've been working as a <b>front-end UI developer</b> for X Earth years.</h4>
+            </div>
+            <div class="nav">
+                <span class="nav__text">Here you'll find more</span>
+                <a
+                    v-on:click="component = 'about'"
+                    class="nav__link"
+                >
+                    <span class="nav__text">about me</span>
+                </a>
+                <span class="nav__text">and some of</span>
+                <a
+                    v-on:click="component = 'works'"
+                    class="nav__link"
+                >
+                    <span class="nav__text">my works</span>
+                </a>
+                <transition name="fade" mode="out-in">
+                    <div
+                        v-if="panel == 1"
+                        key="panel1"
+                    >
+                        <a
+                            href="#"
+                            class="nav__link"
+                            @click.prevent="panel = 2"
+                        >
+                            Some works
+                        </a>
+                    </div>
+                    <div
+                        v-if="panel == 2"
+                        key="panel2"
+                    >
+                        <a
+                            href="#"
+                            class="nav__link"
+                            @click.prevent="panel = 1"
+                        >
+                            My info
+                        </a>
+                    </div>
+                </transition>
+            </div>
+            <transition name="fade" mode="out-in">
+                <div
+                    v-if="panel == 1"
+                    key="panel1"
+                    class="content content--works"
+                >
+                    <div class="grido">
+                        <div class="grido__item">about</div>
+                        <div class="grido__item">about</div>
+                        <div class="grido__item">about</div>
+                        <div class="grido__item">about</div>
+                        <div class="grido__item">about</div>
+                        <div class="grido__item">about</div>
+                    </div>
+                </div>
+                <div
+                    v-if="panel == 2"
+                    key="panel2"
+                    class="content content--about"
+                >
+                    <div class="grido">
+                        <div class="grido__item">works</div>
+                        <div class="grido__item">works</div>
+                        <div class="grido__item">works</div>
+                        <div class="grido__item">works</div>
+                        <div class="grido__item">works</div>
+                        <div class="grido__item">works</div>
+                    </div>
+                </div>
+            </transition>
+            <component v-bind:is="component" />
+        </section>
+        <footer>
+            <ul>
+                <li>linkedIn/in/ivandb</li>
+                <li>email@ivandb.com</li>
+            </ul>
+        </footer>
     </div>
 </template>
 
@@ -43,6 +109,7 @@ export default {
     },
     data(){
         return{
+            panel: 1,
             component: 'about'
         }
     }
