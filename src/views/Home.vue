8c449be085b7bf2407d8e93c3ca61467ed570e50
ivancis <diazianluis@gmail.com>
2019-12-17 23:37:20 +0100

    card plus linter

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,26 @@
         </section>
         <section class="section section--intro">
             <div class="intro-text">
-                <h4>I studied <b>graphic design</b> in Argentina and specialized in <b>digital UI and UX.</b></h4>
-                <h4>Also I've been working as a <b>front-end UI developer</b> for 5 Earth years.</h4>
+                <h4>
+                    I studied <b>graphic design</b> in Argentina and specialized
+                    in <b>digital UI and UX.</b>
+                </h4>
+                <h4>
+                    Also I've been working as a
+                    <b>front-end UI developer</b> for 5 Earth years.
+                </h4>
             </div>
             <div class="nav">
-                <span class="nav__text">Check out the <b v-on:click="component = 'about'" class="nav__link">About</b> section aaand some of my <b v-on:click="component = 'works'" class="nav__link">works</b>.</span>
+                <span class="nav__text"
+                    >Check out the
+                    <b v-on:click="component = 'about'" class="nav__link"
+                        >About</b
+                    >
+                    section aaand some of my
+                    <b v-on:click="component = 'works'" class="nav__link"
+                        >works</b
+                    >.</span
+                >
             </div>
         </section>
         <section class="section section--content">
@@ -22,7 +37,6 @@
 </template>
 
 <script>
-
 import gallery from '@/components/gallery.vue';
 import works from '@/components/works.vue';
 import about from '@/components/about.vue';
@@ -32,13 +46,13 @@ export default {
     components: {
         gallery,
         works,
-        about,
+        about
     },
-    data(){
-        return{
+    data() {
+        return {
             panel: 1,
             component: ''
-        }
+        };
     }
 };
 </script>
