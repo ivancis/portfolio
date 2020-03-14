e63eda613a086a726dee847ee07d178be184b0cf
ivancis <diazianluis@gmail.com>
2020-03-14 19:24:47 +0100

    dunno

--- src/components/works.vue
+++ src/components/works.vue
@@ -1,46 +1,39 @@
 <template>
-    <div class="content content--works">
-        <!-- <div class="gallery-element gallery-element--works">
-            <p>
-                Works
-            </p>
-        </div> -->
-        <div class="grido grido--works">
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Megalotto</h5>
-                </div>
+    <div class="grid-works">
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Megalotto</h5>
             </div>
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Crucijuegos</h5>
-                </div>
+        </div>
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Crucijuegos</h5>
             </div>
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Fucci</h5>
-                </div>
+        </div>
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Fucci</h5>
             </div>
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Garpapp</h5>
-                </div>
+        </div>
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Garpapp</h5>
             </div>
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Proglobal</h5>
-                </div>
+        </div>
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Proglobal</h5>
             </div>
-            <div class="grido__item">
-                <div class="card card--work">
-                    <div class="card-cover" />
-                    <h5 class="card-label">Timekeep</h5>
-                </div>
+        </div>
+        <div class="grid-works__item">
+            <div class="card card--work">
+                <div class="card-cover" />
+                <h5 class="card-label">Timekeep</h5>
             </div>
         </div>
     </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,27 @@
 <template>
     <div class="page page--home">
-        <section class="section section--cover">
-            <div class="intro-text">
-                <h2>Hello, I'm Iván</h2>
-                <h4>I design and build digital products, focused on relevant visual interfaces and user experiences.</h4>
-                <h4>Also this is my <a href="mailto:hello@ivandb.com?Subject=Hello%20Iván" target="_top">email</a>.</h4>
-            </div>
-        </section>
-        <section class="section section--intro">
-            <div class="nav">
-                <span class="nav__text"
-                    >Check out the
-                    <b v-on:click="component = 'about'" class="nav__link"
-                        >About</b
-                    >
-                    section and some of my
-                    <b v-on:click="component = 'works'" class="nav__link"
-                        >works</b
-                    >.</span
-                >
-            </div>
-        </section>
-        <section class="section section--content">
+        <h2>Hello, I'm Iván</h2>
+        <p>
+            I design & build web products, caring for clear, well crafted user interaction.
+            <br/>
+        </p>
+        <nav class="nav">
+            <a
+                class="nav__link"
+                v-on:click="component = 'about'"
+            >
+                About
+            </a>
+            <a
+                v-on:click="component = 'works'"
+                class="nav__link"
+            >
+                Works
+            </a>
+        </nav>
+        <article class="content">
             <component v-bind:is="component" />
-        </section>
+        </article>
     </div>
 </template>
 
